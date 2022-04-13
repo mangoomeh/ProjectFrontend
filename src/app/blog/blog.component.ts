d375537e47182224fa42a3e6c196277357a5b85c
ruibing <rbruibingnet@gmail.com>
2022-04-13 22:32:45 +0800

    added createNewBlogPost() to blog component which create empty blog and redirect to edit page

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../shared/services/auth.service';
+import { BlogService } from '../shared/services/blog.service';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.scss']
+  styleUrls: ['./blog.component.scss'],
 })
 export class BlogComponent implements OnInit {
   public sideBarIsOpen: boolean = false;
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private blogService: BlogService,
+    private authService: AuthService
+  ) {}
 
   toggleSideBar() {
     this.sideBarIsOpen = !this.sideBarIsOpen;
@@ -19,7 +25,23 @@ export class BlogComponent implements OnInit {
     this.router.navigate(['/']);
   }
 
-  ngOnInit(): void {
+  createNewBlogPost() {
+    this.blogService
+      .addBlog({
+        userId: this.authService.getUserId(),
+        title: '',
+        description: '',
+        content: '',
+        blogImgUrl: '',
+        isVisible: true,
+      })
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.router.navigate([`/blog/blogEditor/${res.id}`]);
+        },
+      });
   }
 
+  ngOnInit(): void {}
 }
