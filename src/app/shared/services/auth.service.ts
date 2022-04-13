231f0d688bb6fb97ca7723c6d71bac623be9d81f
ruibing <rbruibingnet@gmail.com>
2022-04-13 15:33:33 +0800

    added decode jwt in auth service

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import * as moment from 'moment';
 import { BaseService } from './base.service';
 
 @Injectable({
@@ -14,6 +13,22 @@ export class AuthService {
 
   login(loginObj: any) {
     return this.http.post<any>(this.baseApiUrl + '/login', loginObj);
+  }
+
+  getDecodedJWT() {
+    const token = this.getToken();
+    const decodedJWT = JSON.parse(window.atob(token?.split('.')[1]));
+    console.log(decodedJWT);
+    return decodedJWT;
+  }
+
+  getUserId() {
+    const decodedJWT = this.getDecodedJWT();
+    return decodedJWT.UserId;
+  }
+
+  getUserRole() {
+    return this.getDecodedJWT().Role;
   }
 
   private getToken() {
