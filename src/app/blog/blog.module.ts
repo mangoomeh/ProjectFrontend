a4f6f59805ab8df1d4d4d0695bd7814e86a604ee
ruibing <rbruibingnet@gmail.com>
2022-04-13 15:33:05 +0800

    Update blog.module.ts

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -5,9 +5,10 @@ import { BlogRoutingModule } from './blog-routing.module';
 import { BlogComponent } from './blog.component';
 import { DiscoverComponent } from './discover/discover.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [BlogComponent, DiscoverComponent, ProfileComponent,],
-  imports: [CommonModule, BlogRoutingModule],
+  imports: [CommonModule, BlogRoutingModule, ReactiveFormsModule],
 })
 export class BlogModule {}
