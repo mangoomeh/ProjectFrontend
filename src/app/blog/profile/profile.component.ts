2f5b8ef61acdced892ac60029fbb1d23d8f995b5
ruibing <rbruibingnet@gmail.com>
2022-04-14 22:52:10 +0800

    clean up codes for profile component

--- src/app/blog/profile/profile.component.ts
+++ src/app/blog/profile/profile.component.ts
@@ -10,20 +10,24 @@ import { UserService } from 'src/app/shared/services/user.service';
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-  imgFile: any;
-  imgUrl: string | ArrayBuffer | null = '';
   editUserForm!: FormGroup;
-  userDetails: any;
-  userRole: string = '';
-  baseApiUrl: string = '';
+  profileImgFile: any;
+  profileImgUrl: string | ArrayBuffer | null = '';
+  baseApiUrl: string = ""
+  fetchedUserObj: any;
+  roleName: string = '';
+  userObj = {
+    fullName: '',
+    email: '',
+    profileImgUrl: '',
+  };
+
   constructor(
     private userService: UserService,
     private formBuilder: FormBuilder,
     private authService: AuthService,
     private baseService: BaseService
-  ) {
-    this.baseApiUrl = this.baseService.baseApiUrl + '/';
-  }
+  ) {}
 
   ngOnInit(): void {
     this.editUserForm = this.formBuilder.group({
@@ -31,14 +35,16 @@ export class ProfileComponent implements OnInit {
       email: ['', Validators.compose([Validators.email, Validators.required])],
     });
     this.getUserDetails();
-    this.userRole = this.authService.getUserRole();
+    this.roleName = this.authService.getUserRole();
+    this.baseApiUrl = this.baseService.baseApiUrl + "/"
   }
 
   getUserDetails() {
     this.userService.getUser(this.authService.getUserId()).subscribe({
       next: (res) => {
-        console.log(res);
-        this.userDetails = res;
+        this.fetchedUserObj = res;
+        this.profileImgUrl =
+          this.baseService.baseApiUrl + '/' + res.profileImgUrl;
         this.editUserForm.controls['fullName'].setValue(res.fullName);
         this.editUserForm.controls['email'].setValue(res.email);
       },
@@ -57,17 +63,17 @@ export class ProfileComponent implements OnInit {
       return;
     }
 
-    if (file.size > maxSize) {
-      alert(`Max size should be ${maxSize / 1000}kb`);
-      // return;
-    }
+    // if (file.size > maxSize) {
+    //   alert(`Max size should be ${maxSize / 1000}kb`);
+    //   return;
+    // }
 
-    this.imgFile = file;
+    this.profileImgFile = file;
 
     const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = (_event) => {
-      this.imgUrl = reader.result;
+      this.profileImgUrl = reader.result;
     };
   }
 
@@ -76,16 +82,18 @@ export class ProfileComponent implements OnInit {
       alert('Invalid Information');
       return;
     }
-    console.log(this.editUserForm.value);
     const formData = new FormData();
-    formData.append("UserDetails", JSON.stringify(this.editUserForm.value));
-    formData.append("UserImage", this.imgFile);
+    this.userObj.fullName = this.editUserForm.value.fullName;
+    this.userObj.email = this.editUserForm.value.email;
+    this.userObj.profileImgUrl = this.fetchedUserObj.profileImgUrl;
+    formData.append('UserDetails', JSON.stringify(this.userObj));
+    formData.append('UserImage', this.profileImgFile);
     this.userService
       .updateUser(this.authService.getUserId(), formData)
       .subscribe({
         next: (res) => {
-          console.log(res);
           this.getUserDetails();
+          document.getElementById('close-modal')?.click();
         },
       });
   }
